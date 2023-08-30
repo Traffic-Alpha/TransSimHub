772c415dd4859b2facd7db68cfb14e201ddd22e9
wmn7 <18811371255@163.com>
2023-08-30 19:10:45 +0800

    add base_sumo_env and tshub_env

---
+++ tshub/tshub_env/base_sumo_env.py
@@ -0,0 +1,199 @@
+'''
+@Author: WANG Maonan
+@Date: 2023-08-23 15:30:01
+@Description: Base tshub Environment
+@LastEditTime: 2023-08-30 17:56:11
+'''
+import sumolib
+from typing import List
+from loguru import logger
+
+from abc import ABC, abstractmethod
+
+class BaseSumoEnvironment(ABC):
+    """
+    Base SUMO Environment for Traffic Signal Control
+
+    :param net_file: (str) SUMO .net.xml file
+    :param route_file: (str) SUMO .rou.xml file
+    :param out_csv_name: (str) name of the .csv output with simulation results. If None no output is generated
+    :param use_gui: (bool) Wheter to run SUMO simulation with GUI visualisation
+    :param begin_time: (int) The time step (in seconds) the simulation starts
+    :param num_seconds: (int) Number of simulated seconds on SUMO. The time in seconds the simulation must end.
+    :param max_depart_delay: (int) Vehicles are discarded if they could not be inserted after max_depart_delay seconds
+    :param delta_time: (int) Simulation seconds between actions
+    :param min_green: (int) Minimum green time in a phase
+    :param max_green: (int) Max green time in a phase
+    :single_agent: (bool) If true, it behaves like a regular gym.Env. Else, it behaves like a MultiagentEnv (https://github.com/ray-project/ray/blob/master/python/ray/rllib/env/multi_agent_env.py)
+    :sumo_seed: (int/string) Random seed for sumo. If 'random' it uses a randomly chosen seed.
+    :fixed_ts: (bool) If true, it will follow the phase configuration in the route_file and ignore the actions. 不调整信号灯
+
+    继承 BaseSumoEnvironment 之后需要重写的:
+    - reset, 初始化 feature 和 agent
+    - computer_observations_rewards, 重写计算特征
+    """
+    def __init__(self, 
+                sumo_cfg:str, # sumo config 文件
+                net_file:str=None, # sumo network 文件
+                route_file:str=None, # sumo route 文件
+                trip_info:str=None, # 是否输出 trip_info, 这里为 trip_info 的路径
+                statistic_output:str=None, # 输出整个仿真过程的统计信息
+                summary:str=None, # 记录每一秒的状态, 这里为文件输出的位置, https://sumo.dlr.de/docs/Simulation/Output/Summary.html, halting 与车辆是否到达终点无关
+                queue_output:str=None, # 记录每一秒, 每一个车道的排队长度, 这里为输出文件位置, https://sumo.dlr.de/docs/Simulation/Output/QueueOutput.html
+                tls_state_add:List=None, # 是否记录 traffic light
+                use_gui:bool=False, # 使用 sumo-gui 或是 sumo
+                is_libsumo:bool=False, # 是否使用 libsumo
+                begin_time=0, 
+                num_seconds=20000, # 最多仿真的时间
+                max_depart_delay=100000, 
+                time_to_teleport=-1, 
+                sumo_seed:str='random', 
+                remote_port:int=None, # 设置端口, 使用 libsumo 不要开启这个
+                num_clients:int=1) -> None:
+        
+        # sumo basic config file
+        self._sumo_cfg = sumo_cfg # sumo 配置文件
+        self._net = net_file # net 文件
+        self._route = route_file # route 文件
+
+        # sumo additional output
+        self.trip_info = trip_info # 输出 trip_info 文件（当堵车的时候会存在问题, 必须车辆达到终点才会别统计）
+        self.statistic_output = statistic_output # 记录总体的信息
+        self.summary = summary # 记录每一秒的综合信息
+        self.queue_output = queue_output # 记录每一个车道的排队长度
+        self.tls_state_add = tls_state_add # 记录信号灯信息
+
+        # 多个 traci 连接
+        self.remote_port = remote_port # 指定端口
+        self.num_clients = num_clients # 连接的数量
+
+        self.use_gui = use_gui # 使用 sumo-gui/sumo
+        if self.use_gui:
+            self._sumo_binary = sumolib.checkBinary('sumo-gui')
+        else:
+            self._sumo_binary = sumolib.checkBinary('sumo')
+
+        self.is_libsumo = is_libsumo # 是否使用 libsumo
+        if self.is_libsumo:
+            self.traci = __import__('libsumo')
+        else:
+            self.traci = __import__('traci')
+        
+        self.begin_time = begin_time
+        self.sim_max_time = num_seconds # 最多的仿真时间
+        self.max_depart_delay = max_depart_delay  # Max wait time to insert a vehicle
+        self.time_to_teleport = time_to_teleport
+        self.sumo_seed = sumo_seed # 设置 sumo 的随机数种子
+        self.sumo = None # self.sumo=traic
+    
+    def _start_simulation(self):
+        """开始仿真
+        """
+        if (self._net == None) and (self._route == None):
+            # 使用 sumocfg 来启动 (没有指定 route 和 net)
+            logger.info('SIM: 使用 sumocfg 来启动 (没有指定 route 和 net)')
+            sumo_cmd = [self._sumo_binary,
+                        '-c', self._sumo_cfg,
+                        '--no-step-log', 'true',
+                        '--no-warnings', 'true',
+                        '--max-depart-delay', str(self.max_depart_delay), 
+                        '--waiting-time-memory', '10000',
+                        '--time-to-teleport', str(self.time_to_teleport)]
+        elif (self._net == None) and (self._route != None): # 指定了 route 文件
+            logger.info(f'SIM: 指定了 route 文件, {self._route}')
+            sumo_cmd = [self._sumo_binary,
+                        '-c', self._sumo_cfg,
+                        '-r', self._route,
+                        '--no-step-log', 'true',
+                        '--no-warnings', 'true',
+                        '--max-depart-delay', str(self.max_depart_delay), 
+                        '--waiting-time-memory', '10000',
+                        '--time-to-teleport', str(self.time_to_teleport)]            
+        else: # 同时指定了 network 和 route
+            logger.info(f'SIM: 同时指定了 {self._net} 和 {self._route} 文件')
+            sumo_cmd = [self._sumo_binary,
+                        '-c', self._sumo_cfg,
+                        '-n', self._net,
+                        '-r', self._route,
+                        '--no-step-log', 'true',
+                        '--no-warnings', 'true',
+                        '--max-depart-delay', str(self.max_depart_delay), 
+                        '--waiting-time-memory', '10000',
+                        '--time-to-teleport', str(self.time_to_teleport)]
+                     
+        if self.begin_time > 0: # 设置开始时间
+            sumo_cmd.append('-b {}'.format(self.begin_time))
+        if self.sumo_seed == 'random': # 随机数种子
+            sumo_cmd.append('--random')
+        else:
+            sumo_cmd.extend(['--seed', str(self.sumo_seed)])
+
+        
+        if self.num_clients > 1: # 设置 num clients
+            logger.debug(f'SIM: Set Num Clients {self.num_clients}.')
+            sumo_cmd.extend(['--num-clients', str(self.num_clients)])    
+
+        if self.use_gui: # 是否使用 gui, start->直接开始仿真; quit-on-end->仿真结束关闭 GUI
+            sumo_cmd.extend(['--start', '--quit-on-end'])
+
+        if self.trip_info is not None: # 使得输出 trip_info
+            sumo_cmd.extend(['--tripinfo-output', self.trip_info])
+        if self.statistic_output is not None: # 使得输出 statistic_output
+            sumo_cmd.extend(['--statistic-output', self.statistic_output])
+        if self.summary is not None: # 使其输出 summary
+            sumo_cmd.extend(['--summary', self.summary])
+        if self.queue_output is not None: # 输出每个车道的排队长度
+            sumo_cmd.extend(['--queue-output', self.queue_output])
+        if self.tls_state_add is not None: # !, 注意, 需要额外去指定探测器, 不然会没有探测器
+            assert isinstance(self.tls_state_add, list), '指定需要的 tls add 文件'
+            sumo_cmd.extend(['-a', ','.join(self.tls_state_add)])
+        if self.is_libsumo: # 使用 libsumo, 需要在不同 process 才可以多开
+            self.traci.start(sumo_cmd)
+            self.sumo = self.traci
+        else: # 使用 traci
+            if self.remote_port is None: # 指定端口
+                logger.debug('SIM: Not Set Port.')
+                self.traci.start(sumo_cmd, label=self.label)
+            else:
+                logger.debug(f'SIM: Set Port {self.remote_port}.')
+                self.traci.start(sumo_cmd, label=self.label, port=self.remote_port)
+                
+            self.sumo = self.traci.getConnection(self.label)
+            if self.num_clients > 1:
+                self.sumo.setOrder(1) # 这里设置为 1
+
+    def _close_simulation(self) -> None:
+        """关闭仿真
+        """
+        if self.sumo is None:
+            return
+        if not self.is_libsumo:
+            self.traci.switch(self.label)
+        self.traci.close()
+        self.sumo = None
+
+    def __del__(self) -> None:
+        self._close_simulation()
+    
+    def _computer_done(self):
+        done = self.sim_step > self.sim_max_time
+        return done
+    
+    @property
+    def sim_step(self):
+        """Return current simulation second on SUMO
+        """
+        return self.sumo.simulation.getTime()
+
+    @abstractmethod
+    def reset(self) -> None:
+        """重置环境, 返回初始的 obs, 需要完成 feature 部分的初始化
+        """
+        pass
+    
+    @abstractmethod
+    def step(self) -> None:
+        pass
+    
+    def render(self, mode=None) -> None:
+        pass
---
+++ tshub/tshub_env/tshub_env.py
@@ -0,0 +1,143 @@
+'''
+@Author: WANG Maonan
+@Date: 2023-08-23 15:34:52
+@Description: 整合 "Veh"（车辆）、"Air"（航空）和 "Traf"（信号灯）的环境
+@LastEditTime: 2023-08-30 18:21:14
+'''
+import os
+import sys
+from typing import Dict, List, Any, Literal
+
+from .base_sumo_env import BaseSumoEnvironment
+from ..aircraft.aircraft_builder import AircraftBuilder
+from ..traffic_light.traffic_light_builder import TrafficLightBuilder
+from ..vehicle.vehicle_builder import VehicleBuilder
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("Please declare the environment variable 'SUMO_HOME'")
+
+
+class TshubEnvironment(BaseSumoEnvironment):
+    """
+    SUMO Environment for Traffic Signal Control
+
+    :param net_file: (str) SUMO .net.xml file
+    :param route_file: (str) SUMO .rou.xml file
+    :param out_csv_name: (str) name of the .csv output with simulation results. If None no output is generated
+    :param use_gui: (bool) Wheter to run SUMO simulation with GUI visualisation
+    :param begin_time: (int) The time step (in seconds) the simulation starts
+    :param num_seconds: (int) Number of simulated seconds on SUMO. The time in seconds the simulation must end.
+    :param max_depart_delay: (int) Vehicles are discarded if they could not be inserted after max_depart_delay seconds
+    :param delta_time: (int) Simulation seconds between actions
+    :param min_green: (int) Minimum green time in a phase
+    :param max_green: (int) Max green time in a phase
+    :single_agent: (bool) If true, it behaves like a regular gym.Env. Else, it behaves like a MultiagentEnv (https://github.com/ray-project/ray/blob/master/python/ray/rllib/env/multi_agent_env.py)
+    :sumo_seed: (int/string) Random seed for sumo. If 'random' it uses a randomly chosen seed.
+    :fixed_ts: (bool) If true, it will follow the phase configuration in the route_file and ignore the actions. 不调整信号灯
+    """
+    CONNECTION_LABEL = 1  # For traci multi-client support
+
+    def __init__(self, 
+                 sumo_cfg: str, 
+                 is_vehicle_builder_initialized:bool = True, 
+                 is_aircraft_builder_initialized:bool = True, 
+                 is_traffic_light_builder_initialized:bool = True,
+                 tls_ids:List[str] = None, aircraft_inits:Dict[str, Any] = None,
+                 vehicle_action_type:str = 'lane', tls_action_type:str = 'next_or_not',
+                 net_file: str = None, route_file: str = None, 
+                 trip_info: str = None, statistic_output: str = None, summary: str = None, queue_output: str = None, 
+                 tls_state_add: List = None, use_gui: bool = False, is_libsumo: bool = False, 
+                 begin_time=0, num_seconds=20000, max_depart_delay=100000, time_to_teleport=-1, 
+                 sumo_seed: str = 'random', 
+                 remote_port: int = None, num_clients: int = 1) -> None:
+        
+        super().__init__(sumo_cfg, net_file, route_file, 
+                         trip_info, statistic_output, summary, queue_output, 
+                         tls_state_add, use_gui, is_libsumo, 
+                         begin_time, num_seconds, max_depart_delay, time_to_teleport, 
+                         sumo_seed, remote_port, num_clients
+                        )
+        
+        self.label = str(TshubEnvironment.CONNECTION_LABEL)
+        TshubEnvironment.CONNECTION_LABEL += 1 # 多次初始化 label 是不同的
+
+        self.is_vehicle_builder_initialized = is_vehicle_builder_initialized
+        self.is_aircraft_builder_initialized = is_aircraft_builder_initialized
+        self.is_traffic_light_builder_initialized = is_traffic_light_builder_initialized
+
+        self.tls_ids = tls_ids
+        self.aircraft_inits = aircraft_inits
+        self.vehicle_action_type = vehicle_action_type
+        self.tls_action_type = tls_action_type
+
+    def __init_builder(self) -> None:
+        vehicle_builder = (
+            VehicleBuilder(sumo=self.sumo, action_type=self.vehicle_action_type)
+            if self.is_vehicle_builder_initialized
+            else None
+        )
+        aircraft_builder = (
+            AircraftBuilder(sumo=self.sumo, aircraft_inits=self.aircraft_inits)
+            if self.is_aircraft_builder_initialized
+            else None
+        )
+        tls_builder = (
+            TrafficLightBuilder(sumo=self.sumo, tls_ids=self.tls_ids, action_type=self.tls_action_type)
+            if self.is_traffic_light_builder_initialized
+            else None
+        )
+
+        self.scene_objects = {
+            'vehicle': vehicle_builder,
+            'aircraft': aircraft_builder,
+            'tls': tls_builder
+        }
+
+    def reset(self) -> Dict[str, Any]:
+        """重置环境, 返回初始的 obs
+        """
+        self._close_simulation() # 关闭仿真
+        self._start_simulation() # 开启仿真
+        self.__init_builder() # 初始化场景内的 builder
+        obs = self.__computer_observation()
+        return obs
+    
+    def step(self, actions):
+        # apply action
+        for _object_type, _object_action in actions.items():
+            if self.scene_objects[_object_type] is not None:
+                self.scene_objects[_object_type].control_objects(_object_action)
+        
+        self.sumo.simulationStep()
+
+        # update env
+        obs = self.__computer_observation()
+        reward = self.__computer_reward()
+        info = self.__compute_info()
+        done = self._computer_done()
+        return obs, reward, info, done
+
+    def __computer_observation(self) -> Dict[str, Any]:
+        """自定义 obs 的计算
+        """
+        env_state = {
+            _object_type: _object_builder.get_objects_infos()
+            for _object_type, _object_builder in self.scene_objects.items()
+            if _object_builder is not None
+        }
+        return env_state
+
+    def __computer_reward(self) -> Literal[0]:
+        """自定义 reward 的计算
+        """
+        return 0
+
+    def __compute_info(self):
+        """每一步, 返回信息
+        """
+        return {
+            'step_time': self.sim_step, # 返回当前仿真的时间
+        }
