7660cf2997a1b20709cf555cab34a2f3f924d66a
wmn7 <18811371255@163.com>
2023-11-14 12:40:17 +0800

    add examples for map utils

--- examples/map/get_net_info.py
+++ examples/map/get_net_info.py
@@ -1,8 +1,8 @@
 '''
 @Author: WANG Maonan
-@Date: 2023-09-22 14:38:20
-@Description: 获得地图中所有的 Polygon 的信息
-@LastEditTime: 2023-11-12 14:27:30
+@Date: 2023-11-12 14:48:21
+@Description: 只获取 net 的信息, 包含 edge 和 node 的 shape
+@LastEditTime: 2023-11-12 14:48:22
 '''
 from loguru import logger
 
@@ -15,13 +15,10 @@ path_convert = get_abs_path(__file__)
 set_logger(path_convert('./'))
 
 net_file = path_convert("../sumo_env/osm_berlin/env/berlin.net.xml")
-poly_file = path_convert("../sumo_env/osm_berlin/env/berlin.poly.xml")
-osm_file = path_convert("../sumo_env/osm_berlin/berlin.osm")
+
 
 map_builder = MapBuilder(
     net_file=net_file,
-    poly_file=poly_file,
-    osm_file=osm_file
 )
 map_infos = map_builder.get_objects_infos()
 logger.info(f'SIM: \n{dict_to_str(map_infos)}')
