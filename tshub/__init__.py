95e8733954cf225963fef60ea04c412f6fe215c2
wmn7 <18811371255@163.com>
2024-05-30 21:50:59 +0800

    update version in init file

--- tshub/__init__.py
+++ tshub/__init__.py
@@ -2,10 +2,10 @@
 @Author: WANG Maonan
 @Date: 2023-08-23 11:02:35
 @Description: tshub
-@LastEditTime: 2023-08-31 11:47:38
+@LastEditTime: 2024-05-30 21:49:33
 '''
 __TITLE__ = 'TransSimHub'
-__VERSION__ = 'v0.6'
+__VERSION__ = 'v0.99'
 __DESCRIPTION__ = 'A lightweight Python library for simulating and controlling transportation systems'
 __AUTHOR__ = "Traffic-Alpha"
 __version__ = __VERSION__
