8d3b3188eddc7ef9c5de8f504293afdea8fccaa9
wmn7 <18811371255@163.com>
2024-07-05 22:15:25 +0800

    [Features] Add 3D Vis in TSHub

---
+++ tshub/visualization/vis3d/vis3d_net/feature.py
@@ -0,0 +1,57 @@
+'''
+@Author: WANG Maonan
+@Date: 2024-07-05 20:29:47
+@Description: Features for SUMO Network (信号灯)
+@LastEditTime: 2024-07-05 20:33:15
+'''
+import numpy as np
+from functools import cached_property
+from typing import Any, List, Optional
+
+from .road_map import RoadMap
+from ..vis3d_utils.coordinates import Point, RefLinePoint
+
+class Feature(RoadMap.Feature):
+    """Feature representation for Sumo road networks
+    """
+    def __init__(
+        self,
+        road_map,
+        feature_id: str,
+        feat_data,
+        feat_type=RoadMap.FeatureType.FIXED_LOC_SIGNAL,
+    ):
+        self._map = road_map
+        self._feature_id = feature_id
+        self._feat_data = feat_data
+        # we only know how to get traffic light signals out of Sumo maps so far...
+        self._type = feat_type
+
+    @property
+    def feature_id(self) -> str:
+        return self._feature_id
+
+    @property
+    def type(self) -> RoadMap.FeatureType:
+        return self._type
+
+    @property
+    def type_as_str(self) -> str:
+        return self._type.name
+
+    @cached_property
+    def geometry(self) -> List[Point]:
+        assert isinstance(self._feat_data, list), f"{self._feat_data}"
+        in_lane = self._map.lane_by_id(self._feat_data[0].getID())
+        stop_pos = in_lane.from_lane_coord(RefLinePoint(s=in_lane.length))
+        return [stop_pos]
+
+    @cached_property
+    def type_specific_info(self) -> Optional[Any]:
+        # the only type we currently handle is FIXED_LOC_SIGNAL
+        in_lane, to_lane, _ = self._feat_data
+        via_id = in_lane.getConnection(to_lane).getViaLaneID()
+        return self._map.lane_by_id(via_id)
+
+    def min_dist_from(self, point: Point) -> float:
+        return np.linalg.norm(self.geometry[0].as_np_array - point.as_np_array)
---
+++ tshub/visualization/vis3d/vis3d_net/glb.py
@@ -0,0 +1,194 @@
+# Copyright (C) 2022. Huawei Technologies Co., Ltd. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import math
+import warnings
+from pathlib import Path
+from typing import Any, Dict, Final, List, Tuple, Union
+
+import numpy as np
+from shapely.geometry import Polygon
+import trimesh.visual
+
+from ..vis3d_utils.coordinates import BoundingBox
+from ..vis3d_utils.geometry import triangulate_polygon
+
+# Suppress trimesh deprecation warning
+with warnings.catch_warnings():
+    warnings.filterwarnings(
+        "ignore",
+        message="Please use `coo_matrix` from the `scipy.sparse` namespace, the `scipy.sparse.coo` namespace is deprecated.",
+        category=DeprecationWarning,
+    )
+    import trimesh  # only suppress the warnings caused by trimesh
+    from trimesh.exchange import gltf
+    from trimesh.visual.material import PBRMaterial
+
+OLD_TRIMESH: Final[bool] = tuple(int(d) for d in trimesh.__version__.split(".")) <= (
+    3,
+    9,
+    29,
+)
+
+
+def _convert_camera(camera):
+    result = {
+        "name": camera.name,
+        "type": "perspective",
+        "perspective": {
+            "aspectRatio": camera.fov[0] / camera.fov[1],
+            "yfov": np.radians(camera.fov[1]),
+            "znear": float(camera.z_near),
+            # HACK: The trimesh gltf export doesn't include a zfar which Panda3D GLB
+            #       loader expects. Here we override to make loading possible.
+            "zfar": float(camera.z_near + 100),
+        },
+    }
+    return result
+
+
+gltf._convert_camera = _convert_camera
+
+
+class GLBData:
+    """Convenience class for writing GLB files.
+    """
+    def __init__(self, bytes_):
+        self._bytes = bytes_
+
+    def write_glb(self, output_path: Union[str, Path]):
+        """Generate a geometry file."""
+        with open(output_path, "wb") as f:
+            f.write(self._bytes)
+
+
+def _generate_meshes_from_polygons(
+    polygons: List[Tuple[Polygon, Dict[str, Any]]]
+) -> List[trimesh.Trimesh]:
+    """Creates a mesh out of a list of polygons.
+    """
+    meshes = []
+
+    # Trimesh's API require a list of vertices and a list of faces, where each
+    # face contains three indexes into the vertices list. Ideally, the vertices
+    # are all unique and the faces list references the same indexes as needed.
+    # TODO: Batch the polygon processing.
+    for poly, metadata in polygons:
+        vertices, faces = [], []
+        point_dict = dict()
+        current_point_index = 0
+
+        # Collect all the points on the shape to reduce checks by 3 times
+        for x, y in poly.exterior.coords:
+            p = (x, y, 0)
+            if p not in point_dict:
+                vertices.append(p)
+                point_dict[p] = current_point_index
+                current_point_index += 1
+        triangles = triangulate_polygon(poly)
+        for triangle in triangles:
+            face = np.array(
+                [point_dict.get((x, y, 0), -1) for x, y in triangle.exterior.coords]
+            )
+            # Add face if not invalid
+            if -1 not in face:
+                faces.append(face)
+
+        if not vertices or not faces:
+            continue
+
+        mesh = trimesh.Trimesh(vertices=vertices, faces=faces, metadata=metadata)
+
+        # Trimesh doesn't support a coordinate-system="z-up" configuration, so we
+        # have to apply the transformation manually.
+        mesh.apply_transform(
+            trimesh.transformations.rotation_matrix(math.pi / 2, [-1, 0, 0])
+        )
+        meshes.append(mesh)
+    return meshes
+
+
+def make_map_glb(
+    polygons: List[Tuple[Polygon, Dict[str, Any]]],
+    bbox: BoundingBox,
+    lane_dividers,
+    edge_dividers,
+) -> GLBData:
+    """Create a GLB file from a list of road polygons.
+    """
+    # Attach additional information for rendering as metadata in the map glb
+    metadata = {
+        "bounding_box": (
+            bbox.min_pt.x,
+            bbox.min_pt.y,
+            bbox.max_pt.x,
+            bbox.max_pt.y,
+        ),
+        "lane_dividers": lane_dividers,
+        "edge_dividers": edge_dividers,
+    }
+    scene = trimesh.Scene(metadata=metadata)
+
+    meshes = _generate_meshes_from_polygons(polygons)
+    material = PBRMaterial(
+        name="RoadDefault",
+        baseColorFactor=(192 / 255, 192 / 255, 192 / 255, 1),
+        metallicFactor=0.8,  # 高金属感
+        roughnessFactor=0.8,  # 低粗糙度，更光滑
+        # normalTexture=Image.open('xxx'), # 可以设置图片
+    )
+
+    for mesh in meshes:
+        mesh.visual.material = material
+        road_id = mesh.metadata["road_id"]
+        lane_id = mesh.metadata.get("lane_id")
+        name = str(road_id)
+        if lane_id is not None:
+            name += f"-{lane_id}"
+        if OLD_TRIMESH:
+            scene.add_geometry(mesh, name, extras=mesh.metadata)
+        else:
+            scene.add_geometry(mesh, name, geom_name=name, metadata=mesh.metadata)
+    return GLBData(gltf.export_glb(scene, include_normals=True))
+
+
+def make_road_line_glb(lines: List[List[Tuple[float, float]]]) -> GLBData:
+    """Create a GLB file from a list of road/lane lines.
+    """
+    scene = trimesh.Scene()
+    material = PBRMaterial(
+        name="NodeDefault",
+        baseColorFactor=(1, 1, 1, 1), # white color
+        metallicFactor=0.8,  # 金属感
+        roughnessFactor=0.8,  # 粗糙度        
+    )
+    for line_pts in lines:
+        vertices = [(*pt, 0.1) for pt in line_pts]
+        point_colors = [(255 / 255, 190 / 255, 40 / 255, 1) for _ in line_pts]
+        point_cloud = trimesh.PointCloud(
+            vertices=vertices,
+            colors=point_colors
+        )
+        point_cloud.visual.material = material
+        point_cloud.apply_transform(
+            trimesh.transformations.rotation_matrix(math.pi / 2, [-1, 0, 0])
+        )
+        scene.add_geometry(point_cloud)
+    return GLBData(gltf.export_glb(scene))
