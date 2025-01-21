de480336332ded8423a389209f8c12496cba1832
wmn7 <18811371255@163.com>
2025-01-21 19:48:58 +0100

    add save_str_to_json in utils

--- tshub/utils/format_dict.py
+++ tshub/utils/format_dict.py
@@ -2,13 +2,13 @@
 @Author: WANG Maonan
 @Date: 2023-08-24 17:34:14
 @Description: 将 dict 转换为字符串, 带有环行
-@LastEditTime: 2023-08-31 20:17:33
+LastEditTime: 2025-01-21 19:48:02
 '''
 import json
 import numpy as np
 
 def dict_to_str(my_dict) -> str:
-    """将字典转换为格式化的JSON字符串
+    """将字典转换为格式化的 JSON 字符串
     """
     def convert_to_serializable(obj):
         if isinstance(obj, np.ndarray):
@@ -17,3 +17,13 @@ def dict_to_str(my_dict) -> str:
 
     json_str = json.dumps(my_dict, indent=4, default=convert_to_serializable)
     return json_str
+
+def save_str_to_json(my_dict, file_path:str, indent:int=4) -> None:
+    """将 dict 串保存为 JSON 文件
+    """
+    try:
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(my_dict, f, ensure_ascii=False, indent=indent)
+        print(f"字典已成功保存到 {file_path}")
+    except Exception as e:
+        print(f"保存失败: {e}")
