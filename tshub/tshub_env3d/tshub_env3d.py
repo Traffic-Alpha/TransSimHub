db8f9f45a9a05052574c9b6140eb3f83cea56b87
wmn7 <18811371255@163.com>
2025-04-15 15:38:15 +0800

    update default parameters

--- tshub/tshub_env3d/tshub_env3d.py
+++ tshub/tshub_env3d/tshub_env3d.py
@@ -5,7 +5,7 @@
 - TshubEnvironment 与 SUMO 进行交互, 获得 SUMO 的数据 (这部分利用 TshubEnvironment)
 - TSHubRenderer 对 SUMO 的环境进行渲染 (这部分利用 TSHubRenderer)
 - TShubSensor 获得渲染的场景的数据, 作为新的 state 进行输出
-LastEditTime: 2025-03-25 18:01:37
+LastEditTime: 2025-04-15 15:37:57
 '''
 from loguru import logger
 from typing import Any, Dict, List, Callable
@@ -46,7 +46,7 @@ class Tshub3DEnvironment(BaseSumoEnvironment3D):
             remote_port: int = None, 
             num_clients: int = 1,
             # TSHubRenderer 的参数
-            preset:str = '480p', 
+            preset:str = '480P', 
             resolution:float = 0.5,
             render_mode: str = "onscreen",
             debuger_print_node:bool = False, # 是否在 reset 的时候打印 node path
