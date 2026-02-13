5aed1479be03a5aca0ab507b5ee7f08f1b694ab4
wmn7 <18811371255@163.com>
2026-02-13 15:08:03 +0800

    update grid info in aircraft in tshub

--- examples/map/get_grid_info.py
+++ examples/map/get_grid_info.py
@@ -2,7 +2,7 @@
 @Author: WANG Maonan
 @Date: 2024-05-29 15:01:25
 @Description: 将地图划分为 grid, 获得每个 grid 每个座标点的信息, 例如 SNR
-@LastEditTime: 2024-05-29 16:37:55
+LastEditTime: 2026-02-13 15:06:46
 '''
 from loguru import logger
 
@@ -22,7 +22,16 @@ map_builder = MapBuilder(
     radio_map_files={'SNIR_100': snir_file}
 )
 map_infos = map_builder.get_objects_infos()
-# 获得信息
-map_infos['grid']['SNIR_100'].get_value_at_coordinate(500, 1000) # 获得每一个点的信息
-map_infos['grid']['SNIR_100'].grid_z # 获得地图的信息
-logger.info(f'SIM: \n{dict_to_str(map_infos)}')
+
+# Get Lane infos in the Map
+logger.info(f'SIM: \n{dict_to_str(map_infos['lane'])}')
+
+# Get Node Infos in the Map
+logger.info(f'SIM: \n{dict_to_str(map_infos['node'])}')
+
+# Get Building Infos in the Map
+logger.info(f'SIM: \n{dict_to_str(map_infos['building'])}')
+
+# Get Grid Infos in the Map
+logger.info(f'SIM: \n{map_infos['grid']['SNIR_100'].get_value_at_coordinate(500, 1000)}') # 获得每一个点的信息
+logger.info(f'SIM: \n{map_infos['grid']['SNIR_100'].grid_z}') # 获得地图的信息
